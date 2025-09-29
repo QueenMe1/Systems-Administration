9373126c9c67ac88908bd827300727de30b732e7
Susan O <110050655+QueenMe1@users.noreply.github.com>
2025-09-28 22:50:32 -0400

    Script revamp

--- Script 2/system_report.py
+++ Script 2/system_report.py
@@ -26,26 +26,25 @@ def main():
     hostname = platform.node()
     logfile = os.path.expanduser(f"~/{hostname}_system_report.log")
 
-    with open(logfile,"w") as file:
-        def write_print(message=""):
-            print(message)
-            file.write(message + "\n")
+    reports = []
+   
 
-    write_print(f"System Report - {datetime.now()}" )
-    write_print("\n")
+    reports.append(f"System Report - {datetime.now()}" )
+    reports.append("\n")
+    reports.append("="*55)
 
     # Find Hostname and Domain Name - Section 1
-    write_print("Device Information \n")
+    reports.append("Device Information \n")
     cmd1 = subprocess.run("hostname", capture_output=True, text= True, shell= True)
     hostname = cmd1.stdout.strip()
     cmd2 = subprocess.run("domainname", capture_output=True, text= True, shell= True)
     domainname = cmd2.stdout.strip()
-    write_print(f"Hostname:                    {hostname}")
-    write_print(f"Domain Name:                    {domainname}")
+    reports.append(f"Hostname:                    {hostname}")
+    reports.append(f"Domain Name:                    {domainname}")
 
-    write_print("\n")
+    reports.append("\n")
     #Section 2 - Network Info
-    write_print("Network Information \n")
+    reports.append("Network Information \n")
     cmd3= subprocess.run(["hostname -I"], capture_output=True, text= True, shell= True)
     ip_addr = cmd3.stdout.strip()
     gateway = get_gateway()
@@ -58,15 +57,15 @@ def main():
     camp2 = camp.split()
     DNS1 = camp2[0]
     DNS2 = camp2[1]
-    write_print(f"IP Address:                      {ip_addr}")
-    write_print(f"Gateway:                         {gateway}")
-    write_print(f"Network Mask:                    {netMask}")
-    write_print(f"DNS1:                            {DNS1}")
-    write_print(f"DNS2:                            {DNS2}")
+    reports.append(f"IP Address:                      {ip_addr}")
+    reports.append(f"Gateway:                         {gateway}")
+    reports.append(f"Network Mask:                    {netMask}")
+    reports.append(f"DNS1:                            {DNS1}")
+    reports.append(f"DNS2:                            {DNS2}")
 
-    write_print("\n")
+    reports.append("\n")
     #Section 3 - OS INFO
-    write_print("Operating System Information \n")
+    reports.append("Operating System Information \n")
     cmd6 = subprocess.run("cat /etc/*release", capture_output=True, text= True, shell= True)
     tr = cmd6.stdout.strip()
     #os name
@@ -81,13 +80,13 @@ def main():
     k = cmd7.stdout.strip()
     k1 = k.split()
     kernel = k1[2]
-    write_print(f"Operating System:                    {os_name}")
-    write_print(f"OS Version:                       {os_v}")
-    write_print(f"Kernel Version:                      {kernel}")
+    reports.append(f"Operating System:                    {os_name}")
+    reports.append(f"OS Version:                       {os_v}")
+    reports.append(f"Kernel Version:                      {kernel}")
 
-    write_print("\n")
+    reports.append("\n")
     # Section 4 - Storage
-    write_print("Storage Information \n")
+    reports.append("Storage Information \n")
     cmd8 =  subprocess.run("df -h /", capture_output=True, text= True, shell= True)
     t = cmd8.stdout.strip()
     t2 = t.split("\n")
@@ -95,13 +94,13 @@ def main():
     size = t3[1]
     used = t3[2]
     free = t3[3]
-    write_print(f"Drive Total:                    {size}")
-    write_print(f"Drive Used:                     {used}")
-    write_print(f"Drive Free:                     {free}")
+    reports.append(f"Drive Total:                    {size}")
+    reports.append(f"Drive Used:                     {used}")
+    reports.append(f"Drive Free:                     {free}")
 
-    write_print("\n")
+    reports.append("\n")
     # Section 5 - Processor Info
-    write_print("Processor Information \n")
+    reports.append("Processor Information \n")
     cmd8 = subprocess.run("lscpu | grep 'Model name'", capture_output=True, text= True, shell= True)
     p = cmd8.stdout.strip()
     p2 = p.split(":")
@@ -113,20 +112,27 @@ def main():
     c1 = cmd0.stdout.strip()
     c2 = cmd_1.stdout.strip()
     num_cores = int(c1) * int(c2)
-    write_print(f"CPU Model:                        {c_name}")
-    write_print(f"Number of Processors:                    {pr}")
-    write_print(f"Number of Cores:                         {num_cores}")
+    reports.append(f"CPU Model:                        {c_name}")
+    reports.append(f"Number of Processors:                    {pr}")
+    reports.append(f"Number of Cores:                         {num_cores}")
 
-    write_print("\n")
+    reports.append("\n")
     # Section 6 - Ram
-    write_print("Memory Information \n")
+    reports.append("Memory Information \n")
     cmd_2 = subprocess.run("free | grep 'Mem:'", capture_output=True, text= True, shell= True)
     lines = cmd_2.stdout
     r = lines.split()
     total = r[1]
     fr_space = r[3]
-    write_print(f"Total RAM:                        {total}")
-    write_print(f"Available RAM:                    {fr_space}")
+    reports.append(f"Total RAM:                        {total}")
+    reports.append(f"Available RAM:                    {fr_space}")
+   
+    for line in reports:
+        print(line)
+   
+    with open(logfile,"w") as f:
+        for line in reports:
+            f.write(line + "\n")
    
 
 
